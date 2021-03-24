ea635a0742025d200c84397cb7eacc531c1f2126
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-03-24 22:14:26 +0600

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,11 @@
+print("Niloy is a good Boy!!!")
+name=input("Who's the good boy?:")
+print("You said, "+name+" is a good boy")
+print("The truth is,NILOY is a good boy.")
+print('''
+Niloy
+Ismail
+Khaled
+Ahsan
+Raiyan
+''')
