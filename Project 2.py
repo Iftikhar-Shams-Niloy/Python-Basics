bf0750516af4cb4d361c36ad1f40f293fd5ae2b2
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-03-25 23:07:33 +0600

    Add files via upload

---
+++ Project 2.py
@@ -0,0 +1,22 @@
+birth_year= input('Enter your birth year:')
+age = 2021 - int(birth_year)
+print("Your age is:")
+print(age)
+print(type(age))
+
+weight_lbs = input('Enter your weight(lbs):')
+weight_kg = float(weight_lbs)* 0.45
+print("Your weight in kg is:")
+print(weight_kg)
+
+name = 'Iftikhar Shams Niloy'
+print(name)
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[0:8])
+print(name[1:7])
+print(name[-5:-1])
