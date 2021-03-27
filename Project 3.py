2238d23ff43f1be73151bc4aa08ff03507731d89
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-03-27 21:39:11 +0600

    Add files via upload

---
+++ Project 3.py
@@ -0,0 +1,35 @@
+def say(name, age):
+    print("Always Have The Lion Attitude, "+ name + age)
+
+print("top")
+
+say("Iftikhar", "18")
+say("Shams", "19")
+say("Niloy", "20")
+
+print("bottom")
+print()
+print("Square value of 2,3,4 respectively:")
+def square(num1):
+    return num1*num1
+
+#SQUARING A NUMBER
+result1 = square(2)
+print(result1)
+result2 = square(3)
+print(result2)
+result3 = square(4)
+print(result3)
+
+#CUBEING A NUMBER
+print()
+print("Cube value of 2,3,4 respectively:")
+
+def cube(num2):
+    return num2*num2*num2
+result4 = cube(2)
+result5 = cube(3)
+result6 = cube(4)
+print(result4)
+print(result5)
+print(result6)
