48d0b37df8bfed3bf81caeb3d4bc93031ffcdd19
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-04-13 20:59:48 +0600

    Add files via upload

---
+++ Fruit Dictionary.py
@@ -0,0 +1,27 @@
+
+fruits = {
+    "Apple" : " An apple is an edible fruit produced\nby an apple tree (Malus domestica).",
+    "Orange" : " The orange is the fruit of various\ncitrus species in the family Rutaceae.",
+    "Grape" : " A grape is a fruit, botanically a berry,\n of the deciduous woody vines of the flowering plant genus Vitis.",
+    "Mango" : "A mango is a stone fruit produced from numerous \nspecies of tropical trees belonging to the flowering plant genus Mangifera, \ncultivated mostly for their edible fruit.",
+    "Jackfruit" : "The jackfruit (Artocarpus heterophyllus), also known as jack tree,\n is a species of tree in the fig, mulberry, and breadfruit family (Moraceae).",
+    "Litchi" : "Litchi chinensis is the sole member of \nthe genus Litchi in the soapberry family,",
+    "Banana" : "A banana is an elongated, edible fruit – botanically a berry[1][2] \n– produced by several kinds of large herbaceous flowering plants in the genus Musa.",
+    "Guava" : "Guava fruits, usually 4 to 12 centimetres (1.6 to 4.7 in) long,\nare round or oval depending on the species. They have a pronounced and typical fragrance,\n similar to lemon rind but less sharp.",
+    "Lemon" : "The lemon, Citrus limon, is a species of small evergreen tree in\n the flowering plant family Rutaceae, native to South Asia, \nprimarily Northeast India.",
+    "Pomegranate" : "The pomegranate (Punica granatum) is a fruit-bearing deciduous\n shrub in the family Lythraceae, subfamily Punicoideae, \nthat grows between 5 and 10 m (16 and 33 ft) tall.",
+    "Watermelaon" : "Watermelon (Citrullus lanatus) is a flowering plant species \nof the Cucurbitaceae family. A scrambling and trailing \nvine-like plant, it was originally domesticated in Africa. \nIt is a highly cultivated fruit worldwide, \nwith more than 1,000 varieties.",
+    "Blackberry" : "The blackberry is an edible fruit produced by any species in \nthe genus Rubus in the family Rosaceae, hybrids among these species\n within the subgenus Rubus, and hybrids between \nthe subgenera Rubus and Idaeobatus.",
+    "Pineapple" : "The pineapple (Ananas comosus) is a tropical plant \nwith an edible fruit and the most economically significant\n plant in the family Bromeliaceae.",
+    "Pear" : "Pears are fruits produced and consumed around the world, \ngrowing on a tree and harvested in late Summer into October.",
+    "Plum" : "A plum is a fruit of some species in Prunus subg. Prunus. \nMature plum fruits may have a dusty-white waxy coating \nthat gives them a glaucous appearance.",
+    "Coconut" : "Botanically, the coconut fruit is a drupe, not a true nut. \nLike other fruits, it has three layers: the exocarp, mesocarp, and endocarp\n. The exocarp and mesocarp make up the 'husk' of the coconuts.",
+    "Carambola" : "Carambola, also known as star fruit or starfruit, is the fruit of Averrhoa carambola, \na species of tree native to tropical Southeast Asia.",
+    "Spondias mombin" : "Spondias mombin, also known as yellow mombin or hog plum is \na species of tree and flowering plant in the family Anacardiaceae. It is native \nto the tropical Americas, including the West Indies.",
+    "Jamrul" : "Chrysophyllum cainito is a tropical tree of the family Sapotaceae. \nIt is native to the Greater Antilles and the West Indies. "
+
+}
+
+fruit_name = input("Enter a fruit name : ")
+print(fruits[fruit_name])
+
