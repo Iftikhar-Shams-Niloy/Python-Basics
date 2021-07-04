bf5286bf81a6c0218653a05d42223fbeefbc481d
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-07-05 02:48:26 +0600

    Update Project : My Calculator.py

--- Project : My Calculator.py
+++ Project : My Calculator.py
@@ -1,35 +1,34 @@
 #Try and Except:
 #Building a Calculator:
-print("$$$[[[Python calculator by NiloY]]]$$$")
-print()
+print("\n$$$[[[Python calculator by NiloY]]]$$$\n")
 print("How to use op/operands:")
 print("1. to add, use + ")
 print("2. to ditract, use -")
 print("3. to devide, use /")
-print("4. to multiply, use *")
-print("5. to exponent(to the power), use **")
+print("4. to multiply, use x")
+print("5. to exponent(to the power), use ^")
 print()
 while(1):
     try:
         num1 = float(input("Enter first number:"))
         op = input("Enter operator/exponent:")
-        num2 = float(input("Enter second number"))
+        num2 = float(input("Enter second number:"))
 
         if op == "+":
-            print(num1 + num2)
+            print("Result :",num1 + num2)
         elif op == "-":
-            print(num1 - num2)
-        elif op == "*":
-            print(num1 * num2)
+            print("Result :",num1 - num2)
+        elif op == "x":
+            print("Result :",num1 * num2)
         elif op == "/":
-            print(num1 / num2)
-        elif op == "**":
-            print(num1**num2)
+            print("Result :",num1 / num2)
+        elif op == "^":
+            print("Result :",num1**num2)
         else:
-            print("Invalid Operator")
+            print("### Invalid Operator!!! ###")
 
     except ValueError:
-        print ("Invalid Input")
+        print ("### Invalid Input!!! ###")
     except ZeroDivisionError:
-        print("Math Error (-_-)")
-
+        print("### Math Error (-_-) ###")
+    print("---------------------------")
