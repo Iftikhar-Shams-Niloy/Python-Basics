d414f26ae6e4f0587e4ab70e41a8cdaceaf47b0b
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-07-24 04:13:56 +0600

    Update 3. All About Input.py

--- 3. All About Input.py
+++ 3. All About Input.py
@@ -10,12 +10,15 @@ print("#CASE 1: "+x1)
 #Defining an Input (CASE 2):
 x2 = int(input("# CASE 2: Enter a number:"))
 print("CASE 2:",x2)
-#Taking multiple input in a line (Case 3):
-a, b, c = list(input("#CASE 3: Enter 3 Words(Using Space): ").split(" "))#Enter 3 numbers using space in between.... Then press enter
-print("CASE 3:{} {} {}".format(a,b,c))
-#Taking multiplle input defining it! (Case 4):
-x, y, z = list(map(int,input("#CASE 4: Enter 3 numbers(Using Space): ").split(" ")))
-print("#CASE 4:",x,y,z)
+#Taking inputs as list elements (Case 3):
+making_list = list(map(int,input("#CASE 3: Enter 3 numbers(Using Space): ").split(" ")))#Here int decides the input type
+print("CASE 3:",making_list)
+#Taking multiple input in a line (Case 4):
+a, b, c = list(input("#CASE 4: Enter 3 Words(Using Space): ").split(" "))#Enter 3 numbers using space in between.... Then press enter
+print("CASE 4:{} {} {}".format(a,b,c))
+#Taking multiplle input defining it! (Case 5):
+x, y, z = list(map(int,input("#CASE 5: Enter 3 numbers(Using Space): ").split(" ")))
+print("#CASE 5:",x,y,z)
 
 ### Ways of uisng Inputs! ###
 print("\n### Ways of using Inputs! ###")
