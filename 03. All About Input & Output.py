d36526ae0aef457ad8d80896559bf7cec5555da3
Iftikhar Shams Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2024-11-07 14:07:54 +0600

    Update 03. All About Input & Output.py
    Added some gaps in between the code lines for better understanding.

--- 03. All About Input & Output.py
+++ 03. All About Input & Output.py
@@ -7,17 +7,22 @@ on the screen'''
 #Taking input (CASE 1):
 x1 = input("# CASE 1: Enter Something:")
 print("#CASE 1: "+x1)
+
 #Defining an Input (CASE 2):
 x2 = int(input("# CASE 2: Enter a number:"))
 print("CASE 2:",x2)
+
 #Taking inputs as list elements (Case 3):
 making_list = list(map(int,input("#CASE 3: Enter 3 numbers[Using coma(,)]: ").split(",")))#Here int decides the input type
+
 #Enter numbers using coma(,) in between.... Then press enter
 print("CASE 3:",making_list)
-#We can also use 'tuple' nstead instead of 'list' to make a tuple with the inputs.
+#We can also use 'tuple' nstead instead of 'list', with the inputs.
+
 #Taking multiple input in a line (Case 4):
 a, b, c = list(input("#CASE 4: Enter 3 Words[Using coma(,)]: ").split(","))#Enter 3 elements using coma(,) in between.... Then press enter
 print("CASE 4:{} {} {}".format(a,b,c))
+
 #Taking multiplle input defining it! (Case 5):
 x, y, z = list(map(int,input("#CASE 5: Enter 3 numbers(Using Space): ").split(" ")))
 print("#CASE 5:",x,y,z)
@@ -32,6 +37,7 @@ print("2. Your name is {}".format(n))
 # Case 3 :
 print("3. Your name is",n)
 # Case 4:
+
 name = input("Enter your name:")
 age = int(input("Enter your age :"))
 print("4_1. Your age is %d" %age)
